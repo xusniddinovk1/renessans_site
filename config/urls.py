11466e3c715e4d86c9f59454eba99c867f827ce6
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-04-04 20:13:58 +0500

    initial commit

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,9 @@
-from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('admin/', include("dashboard.urls")),
     path('', include("lager_app.urls", namespace="lager_app"))
 ]
