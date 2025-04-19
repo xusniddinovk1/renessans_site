bf7aa27df32260ee198d7694031d0b7bb649fece
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-04-19 09:53:03 +0500

    update templates

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,10 @@
+from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),/
+    path('admin/', admin.site.urls),
     path('dashboard/', include("dashboard.urls")),
     # path('', include("lager_app.urls"))
 ]
